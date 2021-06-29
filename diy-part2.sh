e294299128719161aed830f91183ff9c9394d0c7
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-06-29 19:41:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.254.254/g' package/base-files/files/bin/config_generate
 
 # openwrt-vlmcsd
 git clone https://github.com/cokebar/openwrt-vlmcsd.git -b master package/feeds/packages/openwrt-vlmcsd
