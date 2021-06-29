bf01a50ca4f3650dc29761c046b50285539f967a
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-06-29 17:01:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+#helloworld
+sed -i '$a src-git https://github.com/fw876/helloworld.git;master' feeds.conf.default
