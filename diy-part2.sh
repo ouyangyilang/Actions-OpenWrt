8c485bd00116f2bb4ed64a8c34eed65737fa3307
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-06-30 16:25:23 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,16 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.254.254/g' package/base-files/files/bin/config_generate
-
-# openwrt-vlmcsd
-git clone https://github.com/cokebar/openwrt-vlmcsd.git -b master package/feeds/packages/openwrt-vlmcsd
-git clone https://github.com/cokebar/luci-app-vlmcsd.git -b master package/feeds/luci/luci-app-vlmcsd
-
-#helloworld
-git clone https://github.com/fw876/helloworld.git -b master package/feeds/helloworld
-git clone https://github.com/kenzok8/openwrt-packages.git -b master package/feeds/helloworld/openwrt-package
-mv package/feeds/helloworld/openwrt-package/lua-maxminddb microsocks redsocks2 ..
-rm -r -f package/feeds/helloworld/openwrt-package
-git clone https://github.com/kenzok8/small.git -b master package/feeds/helloworld/small
-mv package/feeds/helloworld/small/dns2socks ipt2socks kcptun pdnsd-alt simple-obfs trojan ..
-rm -r -f package/feeds/helloworld/small
