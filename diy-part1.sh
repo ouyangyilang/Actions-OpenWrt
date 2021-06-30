da1516d896c4d0b8c07f98e31f94e1870c12eee8
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-06-30 16:33:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,4 +17,4 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # openwrt-passwall
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' passwall
+sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
