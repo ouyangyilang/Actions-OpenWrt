751aabb761a1b75762c8cba113e835342caad913
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-07-01 08:08:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,14 +11,14 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.254.254/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.254.254/g' package/base-files/files/bin/config_generate
 
 # Host name
-sed -i 's/OpenWrt/MW4530R/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/MW4530R/g' package/base-files/files/bin/config_generate
 
 # Wifi ssid
-sed -i 's/OpenWrt/MW4530R/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/OpenWrt/MW4530R/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # openwrt-vlmcsd
-git clone https://github.com/cokebar/openwrt-vlmcsd.git package/feeds/packages/openwrt-vlmcsd -b master
-git clone https://github.com/cokebar/luci-app-vlmcsd.git package/feeds/luci/luci-app-vlmcsd -b master
+#git clone https://github.com/cokebar/openwrt-vlmcsd.git package/feeds/packages/openwrt-vlmcsd -b master
+#git clone https://github.com/cokebar/luci-app-vlmcsd.git package/feeds/luci/luci-app-vlmcsd -b master
