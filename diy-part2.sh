0aff1010e403b03b9eddcdd98c673beadce0f0a5
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-07-03 07:24:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -32,3 +32,7 @@ sed -i 's/OpenWrt/MW4530R/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 sed -i 's/7d0000/fd0000/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
 sed -i 's/7f0000/ff0000/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
 sed -i 's/8mlzma/16mlzma/g' target/linux/ath79/image/generic.mk
+
+# luci-app-vssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb -b master
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr -b master
