ad469f44b8378fc4314beb5d334995b84444b40c
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2021-07-06 14:53:10 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,7 +23,7 @@ sed -i 's/OpenWrt/MW4530R/g' package/base-files/files/bin/config_generate
 #sed -i "s/timezone='UTC'/zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
 # Enable Wifi
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify default Wifi SSID
 sed -i 's/OpenWrt/MW4530R/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -34,5 +34,5 @@ sed -i 's/7f0000/ff0000/g' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
 sed -i 's/8mlzma/16mlzma/g' target/linux/ath79/image/generic.mk
 
 # luci-app-vssr
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb -b master
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr -b master
+git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb -b master
+git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr -b master
