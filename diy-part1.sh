37cc43b39c7019ff5f28207444a948d8f3593a32
ouyangyilang <48618950+ouyangyilang@users.noreply.github.com>
2022-11-02 23:05:17 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+git clone https://github.com/cokebar/openwrt-vlmcsd package/vlmcsd/openwrt-vlmcsd
+git clone https://github.com/cokebar/luci-app-vlmcsd package/vlmcsd/luci-app-vlmcsd
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+mkdir -p package/helloworld
+for i in "dns2socks" "microsocks" "ipt2socks" "pdnsd-alt" "redsocks2"; do \
+  svn checkout "https://github.com/immortalwrt/packages/trunk/net/$i" "package/helloworld/$i"; \
+done
